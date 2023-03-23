50e2ceaaa8a3c6ee205c472aaa875eb65213b0fc
rwgit2023 <ronangit2023@hotmail.com>
2023-03-22 22:51:18 -0300

    Second Coommit

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
+var consultaCep = fetch('https://viacep.com.br/ws/0100100/json/')
 .then(resposta => resposta.json())
-.then(r => console.log(r)) // fetch é para usar API's externas via link
+.then(r => {
+    if (r.erro){
+        throw erro("Cep não encontrado")
+
+    }else{
+        console.log(r)
+         }
+}   
+    
+    
+
+
+) // fetch é para usar API's externas via link
+.catch(error => console.log(error))
 
 
 console.log(consultaCep)
