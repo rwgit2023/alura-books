effcf6e666af2c42f5957e63b0db58415509aa86
rwgit2023 <ronangit2023@hotmail.com>
2023-03-29 21:30:55 -0300

    Ninith Commit

--- script.js
+++ script.js
@@ -64,6 +64,9 @@ async function buscaEndereco(cep) {
     var logradouro = document.getElementById  ('endereco')                             //Esses itens estão sendo pegos dentro do HTML (Cada campo do questionário)
     var estado = document.getElementById('estado')
 
+    cidade.value = consultaCepConvertida.localidade
+    logradouro.value = consultaCepConvertida.logradouro
+    estado.value = consultaCepConvertida.uf
     console.log(consultaCepConvertida)
     return consultaCepConvertida}
 
